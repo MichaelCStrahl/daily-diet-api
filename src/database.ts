4fa9d09aad98b1d160111a4a91098e9e6e318e89
Michael C. Strahl <michaelstrahl.sm@gmail.com>
2023-07-25 22:37:31 -0300

    Setup project

---
+++ src/database.ts
@@ -0,0 +1,9 @@
+import { knex as setupKnex } from 'knex'
+import { env } from './env'
+
+export const knex = setupKnex({
+  client: 'sqlite',
+  connection: {
+    filename: env.DATABASE_URL,
+  },
+})
