d8023aaaa48a74a9a79d557bcbc00f2625ffafc3
Michael C. Strahl <michaelstrahl.sm@gmail.com>
2023-08-04 10:43:30 -0300

    finished CRUD meals

--- README.md
+++ README.md
@@ -2,15 +2,15 @@
 
 # RF
 
-- [ ] O usuário pode registrar uma refeição feita, com as seguintes informações:
+- [x] O usuário pode registrar uma refeição feita, com as seguintes informações:
     - nome;
     - descrição;
     - data e hora;
     - se a refeição está dentro da dieta ou não;
-- [ ] O usuário pode editar uma refeição, podendo alterar todos os dados acima;
-- [ ] O usuário pode apagar uma refeição;
-- [ ] O usuário pode listar todas as suas refeições;
-- [ ] O usuário pode lista uma única refeição;
+- [x] O usuário pode editar uma refeição, podendo alterar todos os dados acima;
+- [x] O usuário pode apagar uma refeição;
+- [x] O usuário pode listar todas as suas refeições;
+- [x] O usuário pode lista uma única refeição;
 - [ ] O usuário pode recuperar as seguintes métricas:
   - Quantidade total de refeições registradas;
   - Quantidade total de refeições dentro da dieta;
@@ -20,6 +20,6 @@
 # RN
 
 - [x] Dever ser possível criar um usuário;
-- [ ] Deve ser possível identificar os usuários entre as requisições;
-- [ ] Todas refeições devem estar relacionadas a um usuário;
+- [x] Deve ser possível identificar os usuários entre as requisições;
+- [x] Todas refeições devem estar relacionadas a um usuário;
 - [ ] O usuário só pode visualizar, editar e apagar as refeições o qual ele criou;
--- src/routes/meals.ts
+++ src/routes/meals.ts
@@ -4,6 +4,14 @@ import { randomUUID } from 'node:crypto'
 import { knex } from '../database'
 import { checkSessionIdExists } from '../middlewares/check-session-id-exists'
 
+interface Metrics {
+  inDietPercentage: number
+  bestSequency: number
+  total: number
+  totalInDiet: number
+  totalOutDiet: number
+}
+
 export async function mealsRoutes(app: FastifyInstance) {
   app.get('/', { preHandler: [checkSessionIdExists] }, async (request) => {
     const { sessionId } = request.cookies
@@ -16,6 +24,155 @@ export async function mealsRoutes(app: FastifyInstance) {
 
     return { meals }
   })
+
+  app.get('/:id', { preHandler: [checkSessionIdExists] }, async (request) => {
+    const { sessionId } = request.cookies
+
+    const getMealParamsSchema = z.object({
+      id: z.string().uuid(),
+    })
+
+    const { id } = getMealParamsSchema.parse(request.params)
+
+    const userId = await knex('users')
+      .select('id')
+      .where('session_id', sessionId)
+
+    const meal = await knex('meals')
+      .where({
+        id,
+        user_id: userId[0].id,
+      })
+      .first()
+
+    return { meal }
+  })
+
+  app.get(
+    '/metrics',
+    {
+      preHandler: [checkSessionIdExists],
+    },
+    async (request, reply) => {
+      const { sessionId } = request.cookies
+
+      const metrics = {
+        total: 0,
+        bestSequency: 0,
+        inDietPercentage: 0,
+        totalInDiet: 0,
+        totalOutDiet: 0,
+      } as Metrics
+
+      const userId = await knex('users')
+        .select('id')
+        .where('session_id', sessionId)
+
+      const meals = await knex('meals')
+        .where('user_id', userId[0].id)
+        .orderBy('date', 'desc')
+        .select('*')
+
+      if (!meals) {
+        return reply.status(404).send()
+      }
+
+      let auxBestSequency = 0
+
+      meals.map((meal) => {
+        console.log(meal)
+
+        if (meal.is_on_diet === 1) {
+          metrics.totalInDiet++
+          auxBestSequency++
+
+          if (auxBestSequency > metrics.bestSequency) {
+            metrics.bestSequency = auxBestSequency
+          }
+        } else {
+          metrics.totalOutDiet++
+          auxBestSequency = 0
+        }
+
+        return metrics
+      })
+
+      metrics.inDietPercentage = metrics.totalInDiet / metrics.total
+      metrics.total = meals.length
+
+      return { metrics }
+    },
+  )
+
+  app.put(
+    '/:id',
+    { preHandler: [checkSessionIdExists] },
+    async (request, reply) => {
+      const { sessionId } = request.cookies
+
+      const getMealParamsSchema = z.object({
+        id: z.string().uuid(),
+      })
+
+      const { id } = getMealParamsSchema.parse(request.params)
+
+      const getMealBodySchema = z.object({
+        name: z.string().optional(),
+        description: z.string().optional(),
+        date: z.coerce.date().optional(),
+        isOnDiet: z.boolean().optional(),
+      })
+
+      const { name, description, date, isOnDiet } = getMealBodySchema.parse(
+        request.body,
+      )
+
+      const userId = await knex('users')
+        .select('id')
+        .where('session_id', sessionId)
+
+      await knex('meals')
+        .where({
+          id,
+          user_id: userId[0].id,
+        })
+        .update({
+          name,
+          description,
+          date,
+          is_on_diet: isOnDiet,
+        })
+
+      return reply.status(204).send()
+    },
+  )
+
+  app.delete(
+    '/:id',
+    { preHandler: [checkSessionIdExists] },
+    async (request, reply) => {
+      const { sessionId } = request.cookies
+
+      const getMealParamsSchema = z.object({
+        id: z.string().uuid(),
+      })
+
+      const { id } = getMealParamsSchema.parse(request.params)
+
+      const userId = await knex('users')
+        .select('id')
+        .where('session_id', sessionId)
+
+      await knex('meals')
+        .where({
+          id,
+          user_id: userId[0].id,
+        })
+        .del()
+
+      return reply.status(204).send()
+    },
+  )
 
   app.post(
     '/',
